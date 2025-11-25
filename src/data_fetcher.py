734b933780246793d45080a0b2a4b15c690e1f10
DavidG4444 <davidgathumbi97.dg@gmail.com>
2025-11-25 11:19:41 +0300

    completed data fetcher with 1st set of data fetched

--- src/data_fetcher.py
+++ src/data_fetcher.py
@@ -1,4 +1,6 @@
+import os
 import requests
+#print(requests.__version__)
 import pandas as pd
 
 def fetch_binance(symbol="BTCUSDT", interval="1d", limit=1000):
@@ -11,3 +13,17 @@ def fetch_binance(symbol="BTCUSDT", interval="1d", limit=1000):
                   "close_time","quote_asset_volume","num_trades",
                   "taker_base_volume","taker_quote_volume","ignore"]
     return df
+
+df = fetch_binance("BTCUSDT", "1d", 1000)
+print(df.head())
+print(df.info())
+
+def save_raw_csv(df, symbol, interval):
+
+#Save a DataFrame into data/raw/(symbol_interval).csv
+    os.makedirs("data/raw", exist_ok=True)
+    file_path = f"data/raw/{symbol}_{interval}.csv"
+    df.to_csv(file_path, index=False)
+    print(f"Saved: {file_path}")
+
+save_raw_csv(df, "BTCUSDT", "1d")
